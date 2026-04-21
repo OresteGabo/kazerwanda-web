1a8fd5f166fab404fb7bcbf9b7ab022dff79d796
oreste <muhirwa.g.oreste@gmail.com>
2026-04-21 04:30:24 +0200

    Initialize not-found.js to mount NotFound component and set custom 404 page title

---
+++ src/not-found.js
@@ -0,0 +1,8 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import NotFound from './components/NotFound.vue'
+
+document.title = 'Page Not Found | Kaze Rwanda'
+
+createApp(NotFound).mount('#app')
